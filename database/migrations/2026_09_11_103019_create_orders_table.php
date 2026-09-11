3b083c8ec1e842980d0dd5a07160e2ae9296f2ac
Hong Seyhakreach <hongseyhakreach9@gmail.com>
2026-09-11 19:28:19 +0700

    new

--- database/migrations/2026_09_11_103019_create_orders_table.php
+++ database/migrations/2026_09_11_103019_create_orders_table.php
@@ -9,13 +9,17 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('orders', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+   public function up(): void
+{
+    Schema::create('orders', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id') ->constrained('users')->onDelete('cascade');
+        $table->foreignId('address_id')->constrained('addresses')->onDelete('cascade');
+        $table->decimal('total_amount')->required();
+        $table->timestamp('ordered_at')->default(now());
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
